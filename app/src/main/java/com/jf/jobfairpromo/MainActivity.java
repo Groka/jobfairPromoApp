85c98aea5e3ae3de681305e357e8eb76b98a6b5b
Groka <tarik.upss@gmail.com>
2016-09-27 17:41:30 +0200

    Dizaj zavrsen
    Treba provjeriti na sto vise uredjaja

--- app/src/main/java/com/jf/jobfairpromo/MainActivity.java
+++ app/src/main/java/com/jf/jobfairpromo/MainActivity.java
@@ -3,11 +3,12 @@ package com.jf.jobfairpromo;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -20,6 +21,7 @@ public class MainActivity extends Activity {
 
     private Button dugme;
     private EditText etImePrezime, etEmail, etTelefon;
+    private TextView tvSlogan, tvCredits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +30,21 @@ public class MainActivity extends Activity {
 
         // Povezivanje sa UI
 
+
+        tvSlogan = (TextView) findViewById(R.id.jfSlogan);
+        tvCredits = (TextView) findViewById(R.id.credits);
         etImePrezime = (EditText) findViewById(R.id.etImePrezime);
         etEmail = (EditText) findViewById(R.id.etEmail);
         etTelefon = (EditText) findViewById(R.id.etTelefon);
         dugme = (Button) findViewById(R.id.button);
+
+        Typeface jfont = Typeface.createFromAsset(getAssets(), "fonts/RobotoSlab-Regular.ttf");
+        tvSlogan.setTypeface(jfont);
+        tvCredits.setTypeface(jfont);
+        etImePrezime.setTypeface(jfont);
+        etEmail.setTypeface(jfont);
+        etTelefon.setTypeface(jfont);
+        dugme.setTypeface(jfont);
 
         // Kreiranje servisa
 
@@ -47,7 +60,11 @@ public class MainActivity extends Activity {
                 call.enqueue(new Callback<User>() {
                     @Override
                     public void onResponse(Call<User> call, Response<User> response) {
-                        displayError(response.code());
+                        if(etImePrezime.getText().toString().isEmpty() ||
+                                etTelefon.getText().toString().isEmpty() ||
+                                etEmail.getText().toString().isEmpty())
+                            displayError(421);
+                        else displayError(response.code());
                     }
 
                     @Override
@@ -59,12 +76,13 @@ public class MainActivity extends Activity {
         });
 
 
+
     }
 
     private void displayError(int status){
         if(status == 409){
             new AlertDialog.Builder(this)
-                    .setTitle("409")
+                    .setTitle("Postojeci nalog")
                     .setMessage("Već postoji korisnik sa istom email adresom ili brojem telefona")
                     .setNeutralButton("Zatvori", new DialogInterface.OnClickListener() {
                         @Override
@@ -77,8 +95,21 @@ public class MainActivity extends Activity {
         }
         else if(status == 451){
             new AlertDialog.Builder(this)
-                    .setTitle("451")
+                    .setTitle("Email error")
                     .setMessage("Kod nije poslan na Vaš email. Molimo Vas provjerite ispravnost unesene email adrese.")
+                    .setNeutralButton("Zatvori", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }
+        else if(status == 421){
+            new AlertDialog.Builder(this)
+                    .setTitle("Nedostaju podaci")
+                    .setMessage("Morate unijeti sve podatke.")
                     .setNeutralButton("Zatvori", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
